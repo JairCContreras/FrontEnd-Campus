7211bca96c277b4d7fd95ad65a0d52e7776c52b1
Luis Jair C. Cervantes <luis-jair.contreras@atos.net>
2021-12-28 09:42:41 -0600

    objects

--- Javascript Crash Course/index.js
+++ Javascript Crash Course/index.js
@@ -15,10 +15,22 @@ console.log(typeof nulo);
 console.log(typeof indefinido);
 
 
-
+//arrays
 const fruits = ["Apple", "Banana", "Pineapple"];
 const array = new Array (1,3,44,2,1,4);
 console.log(fruits);
 console.log(array);
 
 
+//objects
+const person = {
+    firstName: 'Jair',
+    lastName: 'Contreras',
+    age: 30,
+    hobbies: ['music', 'movies', 'sport'],
+    address: {
+        street: '50 main st',
+        city: 'Bostons',
+        state: 'MA'
+    }
+}
