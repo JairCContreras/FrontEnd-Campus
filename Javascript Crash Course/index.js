45336fb2c4cfcf96a338a4729c762e780591b79a
jair.contrerascer@gmail.com <luis-jair.contreras@atos.net>
2021-12-28 09:33:25 -0600

    valores primitos

---
+++ Javascript Crash Course/index.js
@@ -0,0 +1,26 @@
+//let, const, has a block scope. 
+//types
+const s = 'Hello world';
+const number = 5;
+const decimal = 1.2;
+const boolean = true;
+const nulo = null;
+const indefinido = undefined;
+
+console.log(typeof s);
+console.log(typeof number);
+console.log(typeof decimal);
+console.log(typeof boolean);
+console.log(typeof nulo);
+console.log(typeof indefinido);
+
+
+
+
+
+
+const fruits = ["Apples", "Bananas", "Pears", "Pineapple"]
+
+console.log(s.substring(0,4))
+
+console.log(fruits);
