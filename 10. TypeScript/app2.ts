ab1fc9012a18d5f3140a31bc64a94fbf63b2dd3f
Luis Jair C. Cervantes <luis-jair.contreras@atos.net>
2022-01-03 14:43:35 -0600

    Function types

--- 10. TypeScript/app2.ts
+++ 10. TypeScript/app2.ts
@@ -29,13 +29,17 @@ console.log(r2);
 
 //return types
 
-
 //typescript infer the type
 
-function add(n1: number, n2: number): number{
-    return n1 + n2;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-function printResult(num: number): void{
-    console.log("The result: "+num);
+function printResult(num: number): void {
+  console.log("The result: " + num);
 }
+
+//Function types
+
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
