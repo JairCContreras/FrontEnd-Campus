1710d113e5dbc3ea6ef767c4dafe2b793621ae59
Luis Jair C. Cervantes <luis-jair.contreras@atos.net>
2022-01-04 15:20:39 -0600

    Lisvok principle

--- 11. SOLID/3. Liskov/Liskov.java
+++ 11. SOLID/3. Liskov/Liskov.java
@@ -1,33 +1,41 @@
 interface Animal {
     void walk();
-    void jump() throws Exception;
 }
 
+interface  LightWeightAnimal  extends Animal{
+    void jump();
+}
 
 class Elephant implements Animal {
     @Override
     public void walk(){
         System.out.println("Walking...");
     }
+
+}
+
+class Cat implements LightWeightAnimal {
     @Override
-    public void jump() throws Exception{
-        throw new Exception("Cannot jum");
+    public void walk() {
+        System.out.println("Walking");
+    }
+
+    @Override
+    public void jump() {
+        System.out.println("Jumping");
     }
 }
 
-
 class Liskov  {
-    public static void jumpHole(Animal animal){
-        try {
+    public static void jumpHole(LightWeightAnimal animal){
         animal.walk();
         animal.jump();
         animal.walk();
-        }
-        catch(Exception e){}
-        
     }
     public static void main(String[] args){
         Elephant elefant =  new Elephant();
-        jumpHole(elefant);
+        Cat cat = new Cat();
+    
+        jumpHole(cat);
     }
 }
